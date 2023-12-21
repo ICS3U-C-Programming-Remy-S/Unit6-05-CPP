5408b853f44b774b5fcc17a9bd366bfe0119590f
Remy Skelton <remy.skelton@stu.ocsb.ca>
2023-12-21 04:32:00 +0000

    Done lists.cpp

--- lists.cpp
+++ lists.cpp
@@ -3,11 +3,12 @@
 // Created by: Remy Skelton
 // Date: Dec 17, 2023
 // This program will calculate the average of
-// 10 nums with user input
+// nums with user input
 
 #include <iostream>
 #include <list>
 #include <stdexcept>
+#include <string>
 
 // Function to calculate the average of a list of integers
 double calcAverage(const std::list<int>& listOfMarks) {
@@ -18,47 +19,53 @@ double calcAverage(const std::list<int>& listOfMarks) {
         sum += mark;
     }
 
-    // find avg
-    sum = sum / listOfMarks.size();
+    // Calculate the average
+    double average = sum / listOfMarks.size();
 
     // Return the calculated average
-    return sum;
+    return average;
 }
 
 int main() {
-    // Create a list to store user input marks make other variables
+    // Create a list to store user input marks and other variables
     std::list<int> listOfMarks;
     int userMarkInt;
     double average;
+    std::string userMarkStr;
 
     // Display program information to the user
-    std::cout
-        << "This program calculates the average of the numbers you insert."
-        << std::endl;
+    std::cout <<
+    "This program calculates the average of the numbers you insert."
+    << std::endl;
 
-    do {
+    // For proper catching of errors
+    while (true) {
+        // Get marks as a string
+        std::cout << "Enter a mark between 0 and 100 (use negative to stop): ";
+        std::cin >> userMarkStr;
+
         try {
-            // Get marks
-            std::cout <<
-            "Enter a mark between 0 and 100 (use negative to stop): ";
-            std::cin >> userMarkInt;
+            // Convert userMarkStr to a float
+            userMarkInt = std::stoi(userMarkStr);
 
             // Check if the entered mark is greater than 100
             if (userMarkInt > 100) {
                 std::cout << userMarkInt << " is greater than 100."
                 << std::endl;
+            } else if (userMarkInt < 0) {
+                // break when user wants
+                break;
             } else {
-                // Add the mark to the list
+                // add int to list
                 listOfMarks.push_back(userMarkInt);
             }
         } catch (std::exception) {
-            // Check if input is invalid
-            std::cout << "Invalid input. Please enter a valid integer."
+            // Catch and handle exceptions related to invalid input
+            std::cout << "Invalid input. Please enter a valid number."
             << std::endl;
         }
-    } while (userMarkInt > 0);
-    // remove last input
-    listOfMarks.pop_back();
+    }
+
     // Check if the user entered at least one number
     if (listOfMarks.size() == 0) {
         std::cout << "Please enter a number." << std::endl;
